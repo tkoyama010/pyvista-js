251213711664c9cbc96f0f3011410b15f82e47b8
Tetsuo Koyama <tkoyama010@gmail.com>
2026-02-09 00:30:30 +0900

    Update __init__.py to use metadata from pyproject.toml

--- src/pyvista_js/__init__.py
+++ src/pyvista_js/__init__.py
@@ -4,9 +4,12 @@ This package provides a familiar PyVista interface for 3D visualization
 in browser environments using vtk.js as the rendering backend.
 """
 
-__version__ = "0.1.0"
-__author__ = "Tetsuo Koyama"
-__license__ = "BSD-3-Clause"
+from importlib.metadata import version, metadata
+
+__version__ = version("pyvista-js")
+_metadata = metadata("pyvista-js")
+__author__ = _metadata.get("Author", "Tetsuo Koyama")
+__license__ = _metadata.get("License", "BSD-3-Clause")
 
 from .plotter import Plotter
 from .mesh import Mesh, Sphere, Cube, Cylinder
