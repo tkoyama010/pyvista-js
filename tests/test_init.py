7c92f00153910688306da546fa10d88a08f8eee4
Tetsuo Koyama <tkoyama010@gmail.com>
2026-02-10 23:24:00 +0900

    Bump version to 0.1.1 for release

--- tests/test_init.py
+++ tests/test_init.py
@@ -11,7 +11,7 @@ def test_import():
 
 
 @pytest.mark.parametrize("attr,expected", [
-    ("__version__", "0.2.dev0"),
+    ("__version__", "0.1.1"),
     ("__author__", "Tetsuo Koyama"),
     ("__license__", "BSD-3-Clause"),
 ])
