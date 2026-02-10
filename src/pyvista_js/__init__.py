8a84af67a4011daddea09d939eb7d56490a5ea27
Tetsuo Koyama <tkoyama010@gmail.com>
2026-02-11 01:01:29 +0900

    chore: bump version to 0.1.2

--- src/pyvista_js/__init__.py
+++ src/pyvista_js/__init__.py
@@ -4,7 +4,7 @@ This package provides a familiar PyVista interface for 3D visualization
 in browser environments using vtk.js as the rendering backend.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Tetsuo Koyama"
 __license__ = "BSD-3-Clause"
 
--- tests/test_init.py
+++ tests/test_init.py
@@ -11,7 +11,7 @@ def test_import():
 
 
 @pytest.mark.parametrize("attr,expected", [
-    ("__version__", "0.1.1"),
+    ("__version__", "0.1.2"),
     ("__author__", "Tetsuo Koyama"),
     ("__license__", "BSD-3-Clause"),
 ])
