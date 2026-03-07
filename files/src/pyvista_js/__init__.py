8e8d9b66d3b5013719fb56ed8a7ebfffba6869c4
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-07 00:27:32 +0000

    deploy: 74fac0562841652e165d31d68fa108925f86e05a

--- files/src/pyvista_js/__init__.py
+++ files/src/pyvista_js/__init__.py
@@ -4,9 +4,21 @@ This package provides a familiar PyVista interface for 3D visualization
 in browser environments using vtk.js as the rendering backend.
 """
 
+import sys
+
 __version__ = "0.2.dev0"
 __author__ = "Tetsuo Koyama"
 __license__ = "BSD-3-Clause"
+
+if sys.platform == "emscripten":
+    try:
+        import numpy as np  # noqa: F401
+    except ImportError:
+        import asyncio
+
+        import micropip  # type: ignore[import-not-found]
+
+        asyncio.get_event_loop().run_until_complete(micropip.install("numpy"))
 
 from .mesh import Cube, Cylinder, Mesh, Sphere
 from .plotter import Plotter
