d7d765373550e2db23a8eb5be561dd1c6a0b4749
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-11 15:06:52 +0000

    Deploy preview for PR 110 🛫

---
+++ pr-preview/pr-110/files/src/pyvista_js/js/mesh_source.js
@@ -0,0 +1,4 @@
+const points{{INDEX}} = new Float32Array([{{POINTS_DATA}}]);
+const polydata{{INDEX}} = vtk.Common.DataModel.vtkPolyData.newInstance();
+polydata{{INDEX}}.getPoints().setData(points{{INDEX}}, 3);
+const source{{INDEX}} = polydata{{INDEX}};
