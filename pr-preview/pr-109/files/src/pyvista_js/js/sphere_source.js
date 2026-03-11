54e6efe3f7a17366ed45d189bc6d0a8babb21952
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-11 14:13:22 +0000

    Deploy preview for PR 109 🛫

---
+++ pr-preview/pr-109/files/src/pyvista_js/js/sphere_source.js
@@ -0,0 +1,6 @@
+const source{{INDEX}} = vtk.Filters.Sources.vtkSphereSource.newInstance({
+  center: [{{CENTER_X}}, {{CENTER_Y}}, {{CENTER_Z}}],
+  radius: {{RADIUS}},
+  thetaResolution: {{THETA_RESOLUTION}},
+  phiResolution: {{PHI_RESOLUTION}}
+});
