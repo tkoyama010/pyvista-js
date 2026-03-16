46f808641f066f2e383dfdbb5baf157beaee86af
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-16 15:42:44 +0000

    Deploy preview for PR 157 🛫

--- pr-preview/pr-157/files/src/pyvista_js/__init__.py
+++ pr-preview/pr-157/files/src/pyvista_js/__init__.py
@@ -23,7 +23,7 @@ if sys.platform == "emscripten":
 from . import examples
 from .camera import Camera
 from .light import Light
-from .mesh import Circle, Cone, Cube, Cylinder, Mesh, PolyData, Sphere
+from .mesh import Arrow, Circle, Cone, Cube, Cylinder, Disc, Line, Mesh, Plane, PolyData, Sphere
 from .plotter import Plotter
 from .readers import OBJReader, PLYReader, PolyDataReader
 from .texture import Texture
@@ -33,15 +33,19 @@ try:
     from .streamlit_integration import pyvista_chart
 
     __all__ = [
+        "Arrow",
         "Camera",
         "Circle",
         "Cone",
         "Cube",
         "Cylinder",
+        "Disc",
         "Light",
+        "Line",
         "Mesh",
         "OBJReader",
         "PLYReader",
+        "Plane",
         "Plotter",
         "PolyData",
         "PolyDataReader",
@@ -54,15 +58,19 @@ try:
 except ImportError:
     # Streamlit not available
     __all__ = [
+        "Arrow",
         "Camera",
         "Circle",
         "Cone",
         "Cube",
         "Cylinder",
+        "Disc",
         "Light",
+        "Line",
         "Mesh",
         "OBJReader",
         "PLYReader",
+        "Plane",
         "Plotter",
         "PolyData",
         "PolyDataReader",
