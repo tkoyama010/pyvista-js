5b36c249fa3c7b9a7e557786874e278ed820df69
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-17 06:39:07 +0000

    Remove preview for PR 179 🛬

--- pr-preview/pr-179/files/src/pyvista_js/js/line_source.js
+++
@@ -1,5 +0,0 @@
-const source{{INDEX}} = vtk.Filters.Sources.vtkLineSource.newInstance({
-  point1: [{{POINT_A_X}}, {{POINT_A_Y}}, {{POINT_A_Z}}],
-  point2: [{{POINT_B_X}}, {{POINT_B_Y}}, {{POINT_B_Z}}],
-  resolution: {{RESOLUTION}}
-});
