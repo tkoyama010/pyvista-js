e652454a8e84d71640f7d115ce859dcd06db5455
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-18 14:56:12 +0000

    Remove preview for PR 218 🛬

--- pr-preview/pr-218/files/src/pyvista_js/js/ply_reader_source.js
+++
@@ -1,4 +0,0 @@
-const plyReader{{INDEX}} = vtk.IO.PLY.vtkPLYReader.newInstance();
-const plyBytes{{INDEX}} = Uint8Array.from(atob({{PLY_BASE64}}), c => c.charCodeAt(0));
-plyReader{{INDEX}}.parseAsArrayBuffer(plyBytes{{INDEX}}.buffer);
-const source{{INDEX}} = plyReader{{INDEX}}.getOutputData(0);
