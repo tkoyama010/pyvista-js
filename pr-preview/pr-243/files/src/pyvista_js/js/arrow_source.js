f708b8de11fd9157e939574b88730b9270605c4a
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-19 15:01:01 +0000

    Deploy preview for PR 243 🛫

--- pr-preview/pr-243/files/src/pyvista_js/js/arrow_source.js
+++ pr-preview/pr-243/files/src/pyvista_js/js/arrow_source.js
@@ -1,5 +1,9 @@
-const source{{ INDEX }} = vtk.Filters.Sources.vtkArrowSource.newInstance({
-tipLength: {{ TIP_LENGTH }}, tipRadius: {{ TIP_RADIUS }}, tipResolution:
-{{ TIP_RESOLUTION }}, shaftRadius: {{ SHAFT_RADIUS }}, shaftResolution:
-{{ SHAFT_RESOLUTION }}, direction: [{{ DIR_X }}, {{ DIR_Y }}, {{ DIR_Z }}],
-startAt: 'origin' });
+const source{{INDEX}} = vtk.Filters.Sources.vtkArrowSource.newInstance({
+  tipLength: {{TIP_LENGTH}},
+  tipRadius: {{TIP_RADIUS}},
+  tipResolution: {{TIP_RESOLUTION}},
+  shaftRadius: {{SHAFT_RADIUS}},
+  shaftResolution: {{SHAFT_RESOLUTION}},
+  direction: [{{DIR_X}}, {{DIR_Y}}, {{DIR_Z}}],
+  startAt: 'origin'
+});
--- pr-preview/pr-243/files/src/pyvista_js/js/cylinder_source.js
+++ pr-preview/pr-243/files/src/pyvista_js/js/cylinder_source.js
@@ -1,3 +1,7 @@
-const source{{ INDEX }} = vtk.Filters.Sources.vtkCylinderSource.newInstance({
-center: [{{ CENTER_X }}, {{ CENTER_Y }}, {{ CENTER_Z }}], radius: {{ RADIUS }},
-height: {{ HEIGHT }}, resolution: {{ RESOLUTION }}, generateTCoords: true });
+const source{{INDEX}} = vtk.Filters.Sources.vtkCylinderSource.newInstance({
+  center: [{{CENTER_X}}, {{CENTER_Y}}, {{CENTER_Z}}],
+  radius: {{RADIUS}},
+  height: {{HEIGHT}},
+  resolution: {{RESOLUTION}},
+  generateTCoords: true
+});
