57717d64110551a513cae4550141bf21e105520d
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-20 03:47:01 +0000

    Remove preview for PR 252 🛬

--- pr-preview/pr-252/files/src/pyvista_js/js/sphere_source.js
+++
@@ -1,8 +0,0 @@
-const source{{INDEX}} = vtk.Filters.Sources.vtkSphereSource.newInstance({
-  center: [{{CENTER_X}}, {{CENTER_Y}}, {{CENTER_Z}}],
-  radius: {{RADIUS}},
-  thetaResolution: {{THETA_RESOLUTION}},
-  phiResolution: {{PHI_RESOLUTION}}
-});
-const texMapSphere{{INDEX}} = vtk.Filters.Texture.vtkTextureMapToSphere.newInstance();
-texMapSphere{{INDEX}}.setInputConnection(source{{INDEX}}.getOutputPort());
