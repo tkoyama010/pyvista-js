3b2cad1be216e57e08689f7d6f281370fe8e8ebc
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-20 01:41:13 +0000

    Deploy preview for PR 247 🛫

--- pr-preview/pr-247/files/simple_demo.py
+++ pr-preview/pr-247/files/simple_demo.py
@@ -20,15 +20,15 @@
 # %%
 import micropip
 
-await micropip.install("jinja2")
+await micropip.install("jinja2")  # noqa: F704, PLE1142
 
-import sys
+import sys  # noqa: E402
 
 sys.path.insert(0, "/drive/src")
 
 # %%
-import pyvista_js as pv
-from pyvista_js import examples
+import pyvista_js as pv  # noqa: E402
+from pyvista_js import examples  # noqa: E402
 
 plotter = pv.Plotter()
 mesh = examples.download_bunny()
