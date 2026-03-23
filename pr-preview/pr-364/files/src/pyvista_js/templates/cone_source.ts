a273cf16974b7f1232b58edd0f9cc76310e73a34
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-23 13:15:56 +0000

    Remove preview for PR 364 🛬

--- pr-preview/pr-364/files/src/pyvista_js/templates/cone_source.ts
+++
@@ -1,9 +0,0 @@
-const _source = vtk.Filters.Sources.vtkConeSource.newInstance({
-  center: [centerX, centerY, centerZ],
-  direction: [directionX, directionY, directionZ],
-  height,
-  radius,
-  resolution,
-  capping,
-  generateTCoords: true,
-});
--- pr-preview/pr-364/files/src/pyvista_js/templates/sphere_source.ts
+++
@@ -1,8 +0,0 @@
-const _source = vtk.Filters.Sources.vtkSphereSource.newInstance({
-  center: [centerX, centerY, centerZ],
-  radius,
-  thetaResolution,
-  phiResolution,
-});
-const _texMapSphere = vtk.Filters.Texture.vtkTextureMapToSphere.newInstance();
-_texMapSphere.setInputConnection(_source.getOutputPort());
