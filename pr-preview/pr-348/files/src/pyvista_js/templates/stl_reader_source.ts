edc0ffeb576d9d146b0d00b3420536f55c8b5e99
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-23 08:47:45 +0000

    Deploy preview for PR 348 🛫

---
+++ pr-preview/pr-348/files/src/pyvista_js/templates/stl_reader_source.ts
@@ -0,0 +1,4 @@
+const _reader = vtk.IO.Geometry.vtkSTLReader.newInstance();
+const _bytes = Uint8Array.from(atob(base64Data), (c) => c.charCodeAt(0));
+_reader.parseAsArrayBuffer(_bytes.buffer);
+const _source = _reader.getOutputData(0);
