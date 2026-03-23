58d5869d894dcf9b8b4d618ba6694703d3bd6d4c
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-23 09:19:29 +0000

    Remove preview for PR 348 🛬

--- pr-preview/pr-348/files/src/pyvista_js/templates/arrow_source.ts
+++
@@ -1,9 +0,0 @@
-const _source = vtk.Filters.Sources.vtkArrowSource.newInstance({
-  tipLength,
-  tipRadius,
-  tipResolution,
-  shaftRadius,
-  shaftResolution,
-  direction: [dirX, dirY, dirZ],
-  startAt: "origin",
-});
