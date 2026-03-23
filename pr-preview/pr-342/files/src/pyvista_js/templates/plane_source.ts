91d6dd01e4d318d942576cc93a15fd376b4b9b3b
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-03-23 10:53:29 +0000

    Remove preview for PR 342 🛬

--- pr-preview/pr-342/files/src/pyvista_js/templates/plane_source.ts
+++
@@ -1,7 +0,0 @@
-const _source = vtk.Filters.Sources.vtkPlaneSource.newInstance({
-  origin: [originX, originY, originZ],
-  point1: [point1X, point1Y, point1Z],
-  point2: [point2X, point2Y, point2Z],
-  xResolution: iResolution,
-  yResolution: jResolution,
-});
